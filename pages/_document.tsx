7be285f9a2b23ca18135d62078b672d368d55c20
Agustin Moya <agustinmarcelomoya@gmail.com>
2023-02-22 14:15:13 +0000

    Setup tailwindcss and dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html
+      lang="en"
+      className="bg-white text-black dark:bg-[#111010] dark:text-white"
+    >
       <Head />
       <body>
         <Main />
