db6b1006f3824b6cfe11c757baf48911e0686574
Agustin Moya <agustinmarcelomoya@gmail.com>
2023-02-24 11:42:49 +0000

    Make handler async

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     res.status(401).json({ message: 'Invalid token' });
   }
   try {
-    res.revalidate('/');
+    await res.revalidate('/');
     return res.json({ revalidated: true });
   } catch (error) {
     return res.status(500).send('Error revalidating');
