84733b4ca129bf1aa49384b71ab6006c5e7f2432
Agustin Moya <agustinmarcelomoya@gmail.com>
2023-02-24 11:26:27 +0000

    Revalidate funcionality

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+  try {
+    res.revalidate('/');
+    return res.json({ revalidated: true });
+  } catch (error) {
+    return res.status(500).send('Error revalidating');
+  }
+}
