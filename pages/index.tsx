c90478952554f267cff77eeb2321db296d1bbfb8
Agustin Moya <agustinmarcelomoya@gmail.com>
2023-02-24 11:11:20 +0000

    Add meta tag description

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,7 @@ export default function Gallery({ books }: { books: Book[]; }) {
     <>
       <Head>
         <title>Books gallery</title>
+        <meta name="description" content="This is a project where I used next/image component and fetched data form Supabase" />
       </Head>
       <main className={inter.className}>
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
